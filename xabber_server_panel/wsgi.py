fc56e84e7efb229ffe6444eb1d638e2377e663b6
ilya.basyrov <ilya.basyrov@redsolution.ru>
2024-07-15 10:14:11 +0500

    settings refactored, added version in init file

--- xabber_server_panel/wsgi.py
+++ xabber_server_panel/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'xabber_server_panel.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 application = get_wsgi_application()
